a57ce084c0a88bc83485957106b58d125526def2
Daniel Thorpe <danthorpe@me.com>
2015-12-02 17:08:49 +0000

    [TND-1]: Adds project

---
+++ Sources/Client/Supporting Files/TinodeClient.h
@@ -0,0 +1,17 @@
+//
+//  TinodeClient.h
+//  TinodeClient
+//
+//  Created by Daniel Thorpe on 02/12/2015.
+//
+//
+
+//! Project version number for Tinode Client.
+FOUNDATION_EXPORT double TinodeClientVersionNumber;
+
+//! Project version string for Tinode Client.
+FOUNDATION_EXPORT const unsigned char TinodeClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinodeClient/PublicHeader.h>
+
+
---
+++ Sources/Client/Tests/ClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClientTests.swift
+//  ClientTests
+//
+//  Created by Daniel Thorpe on 02/12/2015.
+//
+//
+
+import XCTest
+@testable import Tinode
+
+class ClientTests: XCTestCase {
+
+    func test__run_test_suite() {
+        XCTAssertTrue(true)
+    }
+}
